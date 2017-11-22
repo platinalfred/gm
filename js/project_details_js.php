ee50f0a15fe224d1b8ab2b650bfe35310393ac5f
ajoluvya <ajoluvya@gmail.com>
2017-11-22 20:03:38 +0300

    Project details report

--- js/project_details_js.php
+++ js/project_details_js.php
@@ -178,76 +178,98 @@ $(document).ready(function(){
 	//deleteDataTableRowData();
 
 	var handleDataTableButtons = function() {
-		/* -- Project Affected Person Data Table --- */
-		if ($("#tblPap").length) {
-			  dTable['tblPap'] = $('#tblPap').DataTable({
-			  dom: "lfrtipB",
-			  "processing": true,
-			  "ajax": {
-				  "url":"getData.php",
-				  "dataType": "JSON",
-				  "type": "POST",
-				  "data":  function(d){
-						d.tbl = 'project_paps';
-						d.project_id = <?php echo $_GET['id']; ?>;
-					}
-			  },"columnDefs": [ {
-				  "targets": [11],
-				  "orderable": false,
-				  "searchable": false
-			  }],
-			  "autoWidth": false,
-			  "footerCallback": function (tfoot, data, start, end, display ) {
-				var api = this.api(), cols = [5,6,7,9,10];
-				$.each(cols, function(key, val){
-					var total = api.column(val).data().sum();
-					$(api.column(val).footer()).html( curr_format(total) );
+		<?php if(isset($projectDetails)):?>
+			/* -- Project Affected Person Data Table --- */
+			//we have to set column indices based on the type of the project
+			var cols = [5,6,7], totals_col = 6; //the index for columns where  the totals appear
+			var last_col = 11; //the final index table column
+			<?php if( !($projectDetails['project_category_unit'] == 2 || $projectDetails['project_category_unit'] == 4) ):?>
+				cols.pop(); last_col--;
+			<?php endif;?>
+			<?php if( !($projectDetails['project_category_unit'] == 1 || $projectDetails['project_category_unit'] == 4) ):?>
+				cols.pop(); last_col--;
+			<?php endif;?>
+			<?php if( !($projectDetails['project_category_unit'] == 5) ):?>
+				cols.pop(); last_col--;
+			<?php endif;?>
+				cols.push(last_col-2,last_col-1);
+			
+			if ($("#tblPap").length) {
+				  dTable['tblPap'] = $('#tblPap').DataTable({
+				  dom: "lfrtipB",
+				  "processing": true,
+				  "ajax": {
+					  "url":"getData.php",
+					  "dataType": "JSON",
+					  "type": "POST",
+					  "data":  function(d){
+							d.tbl = 'project_paps';
+							d.project_id = <?php echo $_GET['id']; ?>;
+						}
+				  },"columnDefs": [ {
+					  "targets": [last_col],
+					  "orderable": false,
+					  "searchable": false
+				  }],
+				  "autoWidth": false,
+				  "footerCallback": function (tfoot, data, start, end, display ) {
+					var api = this.api();
+					$.each(cols, function(key, val){
+						var total = api.column(val).data().sum();
+						$(api.column(val).footer()).html( curr_format(total) );
+					});
+				  },
+				  columns:[ { data: 'pap_ref', render: function( data, type, full, meta ) {return '<a href="project_details.php?id=<?php echo $_GET['id']; ?>&amp;pap_id='+full.id+'" title="View PAP details">'+ data + '</a>';} },
+					  { data: 'firstname', render: function( data, type, full, meta ) {return full.lastname+' ' + data + ' ' + (full.othername?full.othername:'');} },
+						{ data: 'district_name'},
+						/* { data: 'county_id'},
+						{ data: 'subcounty_id'},
+						{ data: 'parish_id'}, */
+						{ data: 'village_id'},
+						{ data: 'phone_contact'},
+						<?php if( $projectDetails['project_category_unit'] == 2 || $projectDetails['project_category_unit'] == 4 ):?>
+						{ data: 'way_leave', render: function( data, type, full, meta ) {return data?curr_format(data):0;}},
+						<?php endif;?>
+						<?php if( $projectDetails['project_category_unit'] == 1 || $projectDetails['project_category_unit'] == 4 ):?>
+						{ data: 'rightofway', render: function( data, type, full, meta ) {return data?curr_format(data):0;}},
+						<?php endif;?>
+						<?php if( $projectDetails['project_category_unit'] == 5 ):?>
+						{ data: 'total_take', render: function( data, type, full, meta ) {return data?curr_format(data):0;}},
+						<?php endif;?>
+						{ data: 'chainage'},
+						{ data: 'improvement_sum', render: function( data, type, full, meta ) {return data?curr_format(parseInt(data)):0;}},
+						{ data: 'crop_tree_sum', render: function( data, type, full, meta ) {return data?curr_format(parseInt(data)):0;}},
+						{ data: 'id', render: function ( data, type, full, meta ) {return '<a data-toggle="modal" data-toggle="modal" href="#papModal" class=" btn-white btn-sm edit_me"><i class="fa fa-pencil"></i> </a><a href="#" class= "btn-danger btn-sm delete_me"><i class="fa fa-trash-o"></i></a>';}}
+						
+						] ,
+				  buttons: [
+					{
+					  extend: "copy",
+					  className: "btn-sm btn-white"
+					},
+					{
+					  extend: "csv",
+					  className: "btn-sm btn-white"
+					},
+					{
+					  extend: "excel",
+					  className: "btn-sm btn-white"
+					},
+					{
+					  extend: "pdfHtml5",
+					  className: "btn-sm btn-white"
+					},
+					{
+					  extend: "print",
+					  className: "btn-sm btn-white"
+					},
+				  ],
+				  responsive: true,
 				});
-			  },
-			  columns:[ { data: 'pap_ref', render: function( data, type, full, meta ) {return '<a href="project_details.php?id=<?php echo $_GET['id']; ?>&amp;pap_id='+full.id+'" title="View PAP details">'+ data + '</a>';} },
-				  { data: 'firstname', render: function( data, type, full, meta ) {return full.lastname+' ' + data + ' ' + (full.othername?full.othername:'');} },
-					{ data: 'district_name'},
-					/* { data: 'county_id'},
-					{ data: 'subcounty_id'},
-					{ data: 'parish_id'}, */
-					{ data: 'village_id'},
-					{ data: 'phone_contact'},
-					{ data: 'way_leave', render: function( data, type, full, meta ) {return data?curr_format(data):0;}},
-					{ data: 'rightofway', render: function( data, type, full, meta ) {return data?curr_format(data):0;}},
-					{ data: 'total_take', render: function( data, type, full, meta ) {return data?curr_format(data):0;}},
-					{ data: 'chainage'},
-					{ data: 'improvement_sum', render: function( data, type, full, meta ) {return data?curr_format(parseInt(data)):0;}},
-					{ data: 'crop_tree_sum', render: function( data, type, full, meta ) {return data?curr_format(parseInt(data)):0;}},
-					{ data: 'id', render: function ( data, type, full, meta ) {return '<a data-toggle="modal" data-toggle="modal" href="#papModal" class=" btn-white btn-sm edit_me"><i class="fa fa-pencil"></i> </a><a href="#" class= "btn-danger btn-sm delete_me"><i class="fa fa-trash-o"></i></a>';}}
-					
-					] ,
-			  buttons: [
-				{
-				  extend: "copy",
-				  className: "btn-sm btn-white"
-				},
-				{
-				  extend: "csv",
-				  className: "btn-sm btn-white"
-				},
-				{
-				  extend: "excel",
-				  className: "btn-sm btn-white"
-				},
-				{
-				  extend: "pdfHtml5",
-				  className: "btn-sm btn-white"
-				},
-				{
-				  extend: "print",
-				  className: "btn-sm btn-white"
-				},
-			  ],
-			  responsive: true,
-			});
-			//$("#datatable-buttons").DataTable();
-		}
-	  /*-- End Project Affected Person DataTable--*/
+				//$("#datatable-buttons").DataTable();
+			}
+		  /*-- End Project Affected Person DataTable--*/
+		<?php endif;?>
 		/* -- PAP Crops Data Table --- */
 		if ($("#tblPapCrop").length) {
 			  dTable['tblPapCrop'] = $('#tblPapCrop').DataTable({
